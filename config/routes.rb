51a91d3a666fd30ac82101b42c8bb9304b304537
houdamzari <houda.mzari1999@gmail.com>
2023-05-02 15:13:20 +0100

    Fix api 500 issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  # Other routes
   root 'home#index'
-  get '/random_greeting', to: 'greetings#random'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get 'greetings/random', to: 'greetings#random'
+    end
+  end
 end
