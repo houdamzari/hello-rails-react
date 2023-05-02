2e1c910a8935102c9252b2c17e61ce5169da2d9d
houdamzari <houda.mzari1999@gmail.com>
2023-05-02 11:48:46 +0100

    Connecting react to rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'home#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
